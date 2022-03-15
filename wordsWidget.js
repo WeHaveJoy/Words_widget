060f54f2f7113f6a11d4f2ef77b74eceed1e1a98
Sinovuyo <cnovuyo10@gmail.com>
2022-03-15 17:05:45 +0200

    added counter button

--- wordsWidget.js
+++ wordsWidget.js
@@ -1,42 +1,66 @@
 const userInput = document.querySelector(".userInput");
 const wordBtn = document.querySelector(".wordBtn");
-const hideHighlightBtn= document.querySelector(".hideHighlightBtn");
+const hideHighlightBtn = document.querySelector(".hideHighlightBtn");
 const message = document.querySelector(".message");
-const highlighted = document.querySelector(".highlighted");
-   
+const countBtn = document.querySelector(".countBtn");
 
-    
 
-function getSentance(){
+
+
+function getSentance() {
     const userSentance = userInput.value;
-    if(userSentance){
+    if (userSentance) {
         return ` ${userSentance}`;
     }
 
 }
 
 
-    function highlightLongWords(userInput){
+function highlightLongWords(userInput) {
 
-        let longestWord = "abcde";
-      
-        const words = userInput.split(" ");
+    // let longestWord = "abcde";
 
+    const words = userInput.split(" ");
+    let sent = ''
 
-        for(let i = 0 ; i < words.length; i++){
+    for (let i = 0; i < words.length; i++) {
 
-            if(words[i].length > longestWord.length){
+        if (words[i].length > 4) {
 
-                userInput.innerHTML = userInput.innerHTML.replace(words[i],`<mark>${words[i]}</mark>`)
+            sent += `<mark  class="and">${words[i]}</mark> `
 
-            }
-        }  
-
+        } else {
+            sent += words[i] + " ";
+        }
     }
 
-    
-  wordBtn.addEventListener('click', function () {
+    message.innerHTML = sent;
+
+}
+
+
+function countWords(userInput) {
+    console.log(userInput);
+    const words = userInput.split(" ");
+    var count = 0;
+    for (var i = 0; i < words.length; i++) {
+        console.log(words[i]);
+        if (words[i] === " ") {
+            count = +1;
+        }
+        count++;
+    }
+    return count;
+
+}
+
+wordBtn.addEventListener('click', function () {
     message.innerText = getSentance();
     highlightLongWords(userInput.value);
-    })
+    countWords(userInput.value);
+})
 
+countBtn.addEventListener('click', function () {
+
+    message.innerHTML = countWords(userInput.value);
+})
